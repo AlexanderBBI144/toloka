91ea94691041b6f0f1a302514e9e8a9eb11912bf
Alexander Somov <alexandree.somov@gmail.com>
2019-12-04 20:26:39 +0300

    Make price level calculations more precise, add is_empty utility func

--- toloka/utils.py
+++ toloka/utils.py
@@ -1,23 +1,33 @@
-from math import factorial
+from math import factorial, ceil
 import numpy as np
+
 
 def _calc_eq(n, q):
     res = 0
-    for k in range(0, n // 2):
+    for k in range(0, ceil(n / 2)):
         res += factorial(n) / factorial(k) / factorial(n-k) * q ** (n - k) * (1 - q) ** k
     return res
 
+
 def _optimize(q, a0):
-    n = 0
+    n = 1
     while _calc_eq(n, q) < a0:
         n += 1
     return n
+
 
 def get_price_levels(p0, levels=[0.8, 0.99], a0=0.99):
     res = {}
     for q in levels:
         q = round(q, 2)
         n = _optimize(q, a0)
-        p = round(p0 / (n-1), 2)
+        p = round(p0 / n, 2)
         res[q] = p
     return res
+
+
+def is_empty(seq):
+     try:
+         return all(is_empty(x) for x in seq) if not isinstance(seq, str) else False
+     except TypeError:
+         return False
